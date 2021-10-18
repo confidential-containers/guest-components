0f70f86d8766fdda2c6c3c4b65dde62512ba8fa9
Jiale Zhang <zhangjiale@linux.alibaba.com>
2021-10-18 16:33:18 +0800

    Modify sample KBC to support CCv0 PoC
    Signed-off-by: Jiale Zhang <zhangjiale@linux.alibaba.com>

--- src/kbc_modules/sample_kbc/mod.rs
+++ src/kbc_modules/sample_kbc/mod.rs
@@ -11,18 +11,22 @@ use anyhow::*;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+const HARDCODED_KEY: &'static [u8] = &[
+    217, 155, 119, 5, 176, 186, 122, 22, 130, 149, 179, 163, 54, 114, 112, 176, 221, 155, 55, 27,
+    245, 20, 202, 139, 155, 167, 240, 163, 55, 17, 218, 234,
+];
+
 // KBS specific packet
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AnnotationPacket {
-    // The access information of KBS is passed to KBC module through annotation.
-    // key_url is used as an example here.
-    pub key_url: String,
-    pub wrapped_key: Vec<u8>,
+    // This is an example annotation packet format.
+    pub kid: String,
+    pub wrapped_data: Vec<u8>,
+    pub iv: Vec<u8>,
     pub wrap_type: String,
 }
 
 pub struct SampleKbc {
-    encrypted_payload: Vec<u8>,
     kbs_info: HashMap<String, String>,
 }
 
@@ -36,18 +40,14 @@ impl KbcInterface for SampleKbc {
     }
 
     fn decrypt_payload(&mut self, annotation: &str) -> Result<Vec<u8>> {
-        // Parse the annotation, and obtain the access information of KBS and the field content to be decrypted.
         let annotation_packet: AnnotationPacket = serde_json::from_str(annotation)?;
-        self.encrypted_payload = annotation_packet.wrapped_key;
+        let encrypted_payload: Vec<u8> = annotation_packet.wrapped_data;
 
-        let cipher_text: &Vec<u8> = &self.encrypted_payload;
-        let decrypting_key = Key::from_slice(b"passphrasewhichneedstobe32bytes!");
-        let cipher = Aes256Gcm::new(decrypting_key);
-        let nonce = Nonce::from_slice(b"unique nonce");
-
-        let plain_text = cipher
-            .decrypt(nonce, cipher_text.as_ref())
-            .map_err(|e| anyhow!("Decrypt failed: {}", e))?;
+        let plain_text = decrypt(
+            &encrypted_payload,
+            HARDCODED_KEY,
+            &annotation_packet.iv,
+        )?;
 
         Ok(plain_text)
     }
@@ -58,8 +58,18 @@ impl SampleKbc {
         let mut kbs_info: HashMap<String, String> = HashMap::new();
         kbs_info.insert("kbs_uri".to_string(), kbs_uri);
         SampleKbc {
-            encrypted_payload: vec![],
             kbs_info: kbs_info,
         }
     }
+}
+
+fn decrypt(encrypted_data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>> {
+    let decrypting_key = Key::from_slice(key);
+    let cipher = Aes256Gcm::new(decrypting_key);
+    let nonce = Nonce::from_slice(iv);
+    let plain_text = cipher
+        .decrypt(nonce, encrypted_data.as_ref())
+        .map_err(|e| anyhow!("Decrypt failed: {:?}", e))?;
+
+    Ok(plain_text)
 }
