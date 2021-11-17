08ee0e7c4c8b93dd82fe27ad0a263fb62ea84b7d
Arron Wang <arron.wang@intel.com>
2021-11-18 01:00:18 +0800

    spec: typo fix
    Signed-off-by: Arron Wang <arron.wang@intel.com>

--- src/spec.rs
+++ src/spec.rs
@@ -4,8 +4,8 @@
 /// MEDIA_TYPE_LAYER_ENC is MIME type used for encrypted layers.
 pub const MEDIA_TYPE_LAYER_ENC: &str = "application/vnd.oci.image.layer.v1.tar+encrypted";
 
-/// MEDIA_TYPE_LAYEE_GZIP_ENC is MIME type used for encrypted compressed layers.
-pub const MEDIA_TYPE_LAYEE_GZIP_ENC: &str = "application/vnd.oci.image.layer.v1.tar+gzip+encrypted";
+/// MEDIA_TYPE_LAYER_GZIP_ENC is MIME type used for encrypted compressed layers.
+pub const MEDIA_TYPE_LAYER_GZIP_ENC: &str = "application/vnd.oci.image.layer.v1.tar+gzip+encrypted";
 
 /// MEDIA_TYPE_LAYER_NON_DISTRIBUTABLE_ENC is MIME type used for non distributable encrypted layers.
 pub const MEDIA_TYPE_LAYER_NON_DISTRIBUTABLE_ENC: &str =
