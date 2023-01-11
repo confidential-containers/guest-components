91e5f2acff33716b47c12fa113b1dfad5f5c0264
Xynnn007 <xynnn@linux.alibaba.com>
2023-01-11 17:26:51 +0800

    Secure Channel: Add support for ttrpc
    `ttrpc` will be a feature, together with `grpc` and `native-aa`.
    Only and exactly one of them should be enabled.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- build.rs
+++ build.rs
@@ -3,8 +3,25 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-fn main() -> std::io::Result<()> {
-    tonic_build::compile_protos("./protos/getresource.proto")?;
+use anyhow::*;
+
+fn main() -> Result<()> {
+    #[cfg(feature = "tonic-build")]
+    tonic_build::compile_protos("./protos/getresource.proto").context("tonic build")?;
+
+    #[cfg(feature = "ttrpc-codegen")]
+    ttrpc_codegen::Codegen::new()
+        .out_dir("./src/secure_channel/ttrpc_proto")
+        .input("./protos/getresource.proto")
+        .include("./protos")
+        .rust_protobuf()
+        .customize(ttrpc_codegen::Customize {
+            async_all: true,
+            ..Default::default()
+        })
+        .rust_protobuf_customize(ttrpc_codegen::ProtobufCustomize::default().gen_mod_rs(false))
+        .run()
+        .context("ttrpc build")?;
 
     Ok(())
 }
