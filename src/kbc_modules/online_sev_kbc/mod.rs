ae05c06466112ad4ef11e9fc425c51ec7ff35c26
Xynnn007 <xynnn@linux.alibaba.com>
2023-02-06 14:01:44 +0800

    feat: online-sev-kbc support get_resource kbs uri
    Now if the request resource `type` in kbs uri is
    `"client-id"`, the responsed resource will be the
    client id.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- src/kbc_modules/online_sev_kbc/mod.rs
+++ src/kbc_modules/online_sev_kbc/mod.rs
@@ -5,14 +5,14 @@
 
 use crate::common::crypto::WrapType;
 use crate::common::{crypto, sev::*};
-use crate::kbc_modules::{KbcCheckInfo, KbcInterface, ResourceDescription, ResourceName};
+use crate::kbc_modules::{KbcCheckInfo, KbcInterface};
+use crate::uri::ResourceUri;
 
 use anyhow::*;
 use async_trait::async_trait;
 use serde::Deserialize;
 use std::collections::HashMap;
 use std::fs;
-use std::str::FromStr;
 use tonic::codegen::http::Uri;
 use uuid::Uuid;
 use zeroize::Zeroizing;
@@ -49,9 +49,7 @@ impl KbcInterface for OnlineSevKbc {
     }
 
     async fn decrypt_payload(&mut self, annotation_packet: AnnotationPacket) -> Result<Vec<u8>> {
-        let key = self
-            .get_key_from_kbs(annotation_packet.kid.resource_path())
-            .await?;
+        let key = self.get_key_from_kbs(annotation_packet.kid).await?;
         let plain_payload = crypto::decrypt(
             key,
             base64::decode(annotation_packet.wrapped_data)?,
@@ -62,17 +60,16 @@ impl KbcInterface for OnlineSevKbc {
         Ok(plain_payload)
     }
 
-    async fn get_resource(&mut self, description: &str) -> Result<Vec<u8>> {
-        let desc = serde_json::from_str::<ResourceDescription>(description)?;
-        match ResourceName::from_str(desc.name.as_str()) {
-            Result::Ok(ResourceName::ClientId) => {
+    async fn get_resource(&mut self, rid: ResourceUri) -> Result<Vec<u8>> {
+        match &rid.r#type[..] {
+            "client-id" => {
                 let connection = self
                     .connection
                     .as_ref()
                     .map_err(|e| anyhow!("Failed to get injected connection. {}", e))?;
                 Ok(connection.client_id.hyphenated().to_string().into_bytes())
             }
-            _ => self.get_resource_from_kbs(desc.name).await,
+            _ => self.get_resource_from_kbs(rid).await,
         }
     }
 }
@@ -126,14 +123,17 @@ impl OnlineSevKbc {
             .to_vec())
     }
 
-    async fn get_key_from_kbs(&self, key_id: String) -> Result<Zeroizing<Vec<u8>>> {
-        let key = self.query_kbs("key".to_string(), key_id).await?;
+    async fn get_key_from_kbs(&self, rid: ResourceUri) -> Result<Zeroizing<Vec<u8>>> {
+        let key = self
+            .query_kbs("key".to_string(), rid.resource_path())
+            .await?;
         let key = Zeroizing::new(key);
         Ok(key)
     }
 
-    async fn get_resource_from_kbs(&self, key_id: String) -> Result<Vec<u8>> {
-        self.query_kbs("resource".to_string(), key_id).await
+    async fn get_resource_from_kbs(&self, rid: ResourceUri) -> Result<Vec<u8>> {
+        self.query_kbs("resource".to_string(), rid.resource_path())
+            .await
     }
 }
 
