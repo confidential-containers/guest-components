824539c2a2e37f164d89cc4868e2a44446e648b4
Xynnn007 <xynnn@linux.alibaba.com>
2023-02-06 14:02:41 +0800

    feat: sample kbc support get_resource kbs uri
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- src/kbc_modules/sample_kbc/mod.rs
+++ src/kbc_modules/sample_kbc/mod.rs
@@ -4,12 +4,12 @@
 //
 
 use crate::common::crypto::{decrypt, WrapType};
-use crate::kbc_modules::{KbcCheckInfo, KbcInterface, ResourceDescription, ResourceName};
+use crate::kbc_modules::{KbcCheckInfo, KbcInterface};
+use crate::uri::ResourceUri;
 
 use anyhow::*;
 use async_trait::async_trait;
 use std::collections::HashMap;
-use std::str::FromStr;
 use zeroize::Zeroizing;
 
 use super::AnnotationPacket;
@@ -18,6 +18,34 @@ const HARDCODED_KEY: &[u8] = &[
     217, 155, 119, 5, 176, 186, 122, 22, 130, 149, 179, 163, 54, 114, 112, 176, 221, 155, 55, 27,
     245, 20, 202, 139, 155, 167, 240, 163, 55, 17, 218, 234,
 ];
+
+#[derive(AsRefStr, EnumString, Display, Debug, PartialEq, Eq)]
+pub enum ResourceType {
+    #[strum(serialize = "security-policy")]
+    /// image security policy, used to define whether a specific
+    /// image can be pulled, or signature verification is needed
+    Policy,
+
+    /// used to configure the storage path of public keys used
+    /// by simple signing when doing iamge signature verification
+    #[strum(serialize = "sigstore-config")]
+    SigstoreConfig,
+
+    /// gpg public key used to verify signature of images in
+    /// simple signing scheme.
+    #[strum(serialize = "gpg-public-config")]
+    GPGPublicKey,
+
+    /// public key file used to verify signature of images in
+    /// cosign scheme.
+    #[strum(serialize = "cosign-public-key")]
+    CosignVerificationKey,
+
+    /// container registry auth file, used to provide auth
+    /// when accessing a private registry / repository
+    #[strum(serialize = "credential")]
+    Credential,
+}
 
 pub struct SampleKbc {
     kbs_info: HashMap<String, String>,
@@ -45,28 +73,18 @@ impl KbcInterface for SampleKbc {
         Ok(plain_text)
     }
 
-    async fn get_resource(&mut self, description: &str) -> Result<Vec<u8>> {
-        let desc: ResourceDescription = serde_json::from_str::<ResourceDescription>(description)?;
-
-        match ResourceName::from_str(desc.name.as_str()) {
-            Result::Ok(ResourceName::Policy) => {
-                Ok(std::include_str!("policy.json").as_bytes().to_vec())
-            }
-            Result::Ok(ResourceName::SigstoreConfig) => {
-                Ok(std::include_str!("sigstore_config.yaml")
-                    .as_bytes()
-                    .to_vec())
-            }
-            Result::Ok(ResourceName::GPGPublicKey) => {
-                Ok(std::include_str!("pubkey.gpg").as_bytes().to_vec())
-            }
-            Result::Ok(ResourceName::CosignVerificationKey) => {
+    async fn get_resource(&mut self, rid: ResourceUri) -> Result<Vec<u8>> {
+        let typ = ResourceType::try_from(&rid.r#type[..])?;
+        match typ {
+            ResourceType::Policy => Ok(std::include_str!("policy.json").as_bytes().to_vec()),
+            ResourceType::SigstoreConfig => Ok(std::include_str!("sigstore_config.yaml")
+                .as_bytes()
+                .to_vec()),
+            ResourceType::GPGPublicKey => Ok(std::include_str!("pubkey.gpg").as_bytes().to_vec()),
+            ResourceType::CosignVerificationKey => {
                 Ok(std::include_str!("cosign.pub").as_bytes().to_vec())
             }
-            Result::Ok(ResourceName::Credential) => {
-                Ok(std::include_str!("auth.json").as_bytes().to_vec())
-            }
-            _ => Err(anyhow!("Unknown resource name")),
+            ResourceType::Credential => Ok(std::include_str!("auth.json").as_bytes().to_vec()),
         }
     }
 }
