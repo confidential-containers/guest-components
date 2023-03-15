7ef4d7b356122ba0c77d5f3be157e8d441061b96
Xynnn007 <xynnn@linux.alibaba.com>
2023-03-15 22:49:54 +0800

    KeyProvider: use Cli parser instead of matches
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- sample_keyprovider/src/main.rs
+++ sample_keyprovider/src/main.rs
@@ -4,12 +4,33 @@
 //
 
 use anyhow::*;
-use clap::{App, Arg};
+use clap::{arg, command, Parser};
 use log::*;
-use std::net::SocketAddr;
+use std::{net::SocketAddr, path::PathBuf};
 
 pub mod enc_mods;
 pub mod grpc;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Socket address (IP:port) to listen to, e.g. 127.0.0.1:50000.
+    #[arg(required = true, short, long)]
+    socket: SocketAddr,
+
+    /// Private key used to authenticate the resource registration endpoint token (JWT)
+    /// to Key Broker Service. This key can sign legal JWTs. If both `kbs`
+    /// and this field are given, the automatic registration will be
+    /// enabled.
+    #[arg(short, long)]
+    auth_private_key: Option<PathBuf>,
+
+    /// Address of Key Broker Service. If both `auth_private_key` and
+    /// this field are specified, the keys generated to encrypt an image
+    /// will be automatically registered into the KBS.
+    #[arg(long)]
+    kbs: Option<SocketAddr>,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -27,13 +48,10 @@ async fn main() -> Result<()> {
         )
         .get_matches();
 
-    let socket = app_matches
-        .value_of("socket addr")
-        .unwrap_or("127.0.0.1:50000")
-        .parse::<SocketAddr>()?;
+    let cli = Cli::parse();
 
     debug!("starting keyprovider gRPC service...");
-    debug!("listening to socket addr: {:?}", socket);
+    info!("listening to socket addr: {:?}", cli.socket);
 
     grpc::start_service(socket).await?;
 
