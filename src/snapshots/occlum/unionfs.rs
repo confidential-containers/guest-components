910a9a2d9d5f56b172966799c1eee4db3d6c2eeb
ppalczew <piotr.pawel.palczewski@intel.com>
2023-06-21 08:17:17 +0000

    Forgot sefs directory

--- src/snapshots/occlum/unionfs.rs
+++ src/snapshots/occlum/unionfs.rs
@@ -168,7 +168,7 @@ impl Snapshotter for Unionfs {
         info!("Moving to create file here");
         read_files_in_home_dir();
         let file_create_path_1 = mount_path.join("foo.txt"); //Path::new("/tmp/coco/agent/rootfs/images/test/foo.txt");
-        let file_create_path_2 = mount_path.join("lower").join("foo.txt"); //Path::new("/tmp/coco/agent/rootfs/images/test/foo.txt");
+        let file_create_path_2 = mount_path.join("sefs").join("lower").join("foo.txt"); //Path::new("/tmp/coco/agent/rootfs/images/test/foo.txt");
         let file_create_path_3 = Path::new("/etc").join("foo.txt"); //Path::new("/tmp/coco/agent/rootfs/images/test/foo.txt");
         create_example_file(&PathBuf::from(&file_create_path_1))
             .map_err(|e| {
