ec886fd844d20f84a4e46ba650f5f76165f6675e
Mikko Ylinen <mikko.ylinen@intel.com>
2023-06-22 12:20:39 +0300

    aa: test-binaries: fix Occlum tester
    SGX DCAP Attester checks that the evidence passed in is base64
    encoded SHA384 and fails if the base64 decoded input does not
    match the expected length.
    
    Therefore, "test".into() won't work and errors. Fix by using
    the same hasher as the KBS protocol.
    
    Signed-off-by: Mikko Ylinen <mikko.ylinen@intel.com>

--- attestation-agent/test-binaries/src/bin/occlum-attester.rs
+++ attestation-agent/test-binaries/src/bin/occlum-attester.rs
@@ -5,10 +5,11 @@
 
 use anyhow::*;
 use attester::{sgx_dcap::SgxDcapAttester, Attester};
+use crypto::hash_chunks;
 
 fn real_main() -> Result<String> {
     let sgx_attester = SgxDcapAttester {};
-    sgx_attester.get_evidence("test".into())
+    sgx_attester.get_evidence(hash_chunks(vec!["test".as_bytes().to_vec()]))
 }
 
 fn main() {
