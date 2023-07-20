a398c5bd46004f50481404532a6909060b876a73
Xynnn007 <xynnn@linux.alibaba.com>
2023-07-20 13:52:01 +0800

    CDH: add secret crate
    This PR only has definition for sealed secrets.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

---
+++ confidential-datahub/secret/src/error.rs
@@ -0,0 +1,11 @@
+// Copyright (c) 2023 Alibaba Cloud
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Error, Debug)]
+pub enum Error {}
---
+++ confidential-datahub/secret/src/secret/layout/envelope.rs
@@ -0,0 +1,41 @@
+// Copyright (c) 2023 Alibaba Cloud
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+use std::collections::HashMap;
+
+use crypto::WrapType;
+use serde::{Deserialize, Serialize};
+
+/// An Envelope is a secret encrypted by digital envelope mechanism.
+/// It can be described as
+///
+/// {Enc(KMS, DEK), Enc(DEK, secret), paras...}
+///
+/// where Enc(A,B) means use key A to encrypt B
+///
+/// The fields inside this Struct will be flattened in a Secret wrapper.
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct Envelope {
+    /// key id to locate the key inside KMS
+    pub key_id: String,
+
+    /// Encrypted DEK by key inside KMS
+    pub encrypted_key: String,
+
+    /// Encrypted data (secret) by DEK
+    pub encrypted_data: String,
+
+    /// Encryption scheme of the Encrypted data by DEK
+    pub wrap_type: WrapType,
+
+    /// IV of encrypted_data, if used
+    pub iv: String,
+
+    /// decryptor driver of the secret
+    pub provider: String,
+
+    /// KMS specific fields to locate the Key inside KMS
+    pub annotations: HashMap<String, String>,
+}
