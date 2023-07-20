bb407297616a14f5be28c316672bad33db861fea
Xynnn007 <xynnn@linux.alibaba.com>
2023-07-20 13:12:50 +0800

    CDH: add confidential datahub crate
    This commit defines the basic API for confidential datahub and the basic
    error handling.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

---
+++ confidential-datahub/hub/src/api.rs
@@ -0,0 +1,29 @@
+// Copyright (c) 2023 Alibaba Cloud
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+use async_trait::async_trait;
+
+use crate::Result;
+
+/// The APIs of the DataHub. See
+/// <https://github.com/confidential-containers/documentation/issues/131> for
+/// more information.
+#[async_trait]
+pub trait DataHub {
+    /// Unseal the given sealed secret. The sealed secret format is defined
+    /// in <https://github.com/confidential-containers/guest-components/blob/main/confidential-datahub/docs/SEALED_SECRET.md>
+    async fn unseal_secret(&self, secret: Vec<u8>) -> Result<Vec<u8>>;
+
+    /// Unwrap the LEK inside the image annotation. This API is used in
+    /// `ocicrypt`'s `KeyProvider`. The received parameter should be an
+    /// AnnotationPacket. Please refer to
+    /// <https://github.com/confidential-containers/guest-components/blob/main/attestation-agent/docs/IMAGE_ENCRYPTION.md#annotation-packet>
+    async fn unwrap_key(&self, annotation: &[u8]) -> Result<Vec<u8>>;
+
+    /// Get the resource due to the given KBS Resource URI. The KBS Resource
+    /// URI is defined in
+    /// <https://github.com/confidential-containers/guest-components/blob/main/attestation-agent/docs/KBS_URI.md>
+    async fn get_resource(&self, uri: String) -> Result<Vec<u8>>;
+}
