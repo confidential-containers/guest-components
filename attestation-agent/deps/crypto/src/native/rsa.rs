9a6f3bfcc2317bedff1a8c5f08d045946f38afdd
Xynnn007 <xynnn@linux.alibaba.com>
2023-07-30 15:27:29 +0800

    dep/crypto: add rsa impl in openssl
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- attestation-agent/deps/crypto/src/native/rsa.rs
+++ attestation-agent/deps/crypto/src/native/rsa.rs
@@ -8,8 +8,9 @@ use openssl::{
     pkey::Private,
     rsa::{Padding, Rsa},
 };
+use zeroize::Zeroizing;
 
-use crate::PaddingMode;
+use crate::rsa::{PaddingMode, RSA_PUBKEY_LENGTH};
 
 #[derive(Debug, Clone)]
 pub struct RSAKeyPair {
@@ -18,13 +19,13 @@ pub struct RSAKeyPair {
 
 impl RSAKeyPair {
     pub fn new() -> Result<RSAKeyPair> {
-        let private_key = Rsa::generate(2048)?;
+        let private_key = Rsa::generate(RSA_PUBKEY_LENGTH as u32)?;
         Ok(Self { private_key })
     }
 
     pub fn decrypt(&self, mode: PaddingMode, cipher_text: Vec<u8>) -> Result<Vec<u8>> {
-        let mut plaintext = Vec::new();
-        match mode {
+        let mut plaintext = [0; RSA_PUBKEY_LENGTH];
+        let decrypted_size = match mode {
             PaddingMode::OAEP => self
                 .private_key
                 .private_decrypt(&cipher_text, &mut plaintext, Padding::PKCS1_OAEP)
@@ -34,7 +35,8 @@ impl RSAKeyPair {
                 .private_decrypt(&cipher_text, &mut plaintext, Padding::PKCS1)
                 .map_err(|e| anyhow!("RSA key pkcs1v15 decrypt failed: {:?}", e))?,
         };
-        Ok(plaintext)
+
+        Ok(plaintext[..decrypted_size].to_vec())
     }
 
     pub fn n(&self) -> Vec<u8> {
@@ -43,5 +45,17 @@ impl RSAKeyPair {
 
     pub fn e(&self) -> Vec<u8> {
         self.private_key.e().to_vec()
+    }
+
+    pub fn to_pkcs1_pem(&self) -> Result<Zeroizing<String>> {
+        let res = self.private_key.private_key_to_pem()?;
+        let pem = String::from_utf8(res)?;
+        Ok(Zeroizing::new(pem))
+    }
+
+    pub fn from_pkcs1_pem(pem: &str) -> Result<Self> {
+        let private_key = Rsa::<Private>::private_key_from_pem(pem.as_bytes())?;
+
+        Ok(Self { private_key })
     }
 }
