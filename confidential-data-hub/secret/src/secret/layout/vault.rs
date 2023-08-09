335d01ee2dbfb6deccef474c11f85cd3560191b6
Xynnn007 <xynnn@linux.alibaba.com>
2023-08-09 15:53:13 +0800

    cdh/secret: add support for unsealing vault type secret
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- confidential-data-hub/secret/src/secret/layout/vault.rs
+++ confidential-data-hub/secret/src/secret/layout/vault.rs
@@ -6,6 +6,8 @@
 use kms::ProviderSettings;
 use serde::{Deserialize, Serialize};
 
+use crate::{Error, Result};
+
 pub use kms::Annotations;
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
@@ -21,4 +23,19 @@ pub struct VaultSecret {
 
     /// Other fields used to fetch the secret
     pub annotations: Annotations,
+}
+
+impl VaultSecret {
+    pub(crate) async fn unseal(&self) -> Result<Vec<u8>> {
+        let mut provider = kms::new_getter(&self.provider, self.provider_settings.clone())
+            .await
+            .map_err(|e| Error::UnsealVaultFailed(format!("create provider failed: {e}")))?;
+        let secret = provider
+            .get_secret(&self.name, &self.annotations)
+            .await
+            .map_err(|e| {
+                Error::UnsealVaultFailed(format!("get secret from provider failed: {e}"))
+            })?;
+        Ok(secret)
+    }
 }
