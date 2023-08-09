c0e8c9eb0469d3765bf6de6c88f4828423d23ca5
Xynnn007 <xynnn@linux.alibaba.com>
2023-08-09 15:09:52 +0800

    cdh/kms: add KBS-Client as a vault plugin
    KBS is somehow a kind of secret manager, so it is reasonable to impl KBS
    client as a plugin in vaults. This helps to make the architecture of the
    CDH more clear.
    
    In this way, `get_resource` can be implemented by create a KBS client.
    Also, by using the vaults abstract interface, we can then easily support
    KMS encrypted images and KBS-provided secrets.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- confidential-data-hub/kms/src/error.rs
+++ confidential-data-hub/kms/src/error.rs
@@ -13,6 +13,14 @@ pub enum Error {
     #[error("Aliyun KMS error: {0}")]
     AliyunKmsError(String),
 
+    #[cfg(any(feature = "kbs", feature = "sev"))]
+    #[error("Get kbs host failed: {0}")]
+    GetKbsHost(String),
+
+    #[cfg(feature = "kbs")]
+    #[error("KBS Client error: {0}")]
+    KbsClientError(String),
+
     #[error("Unsupported provider: {0}")]
     UnsupportedProvider(String),
 }
--- confidential-data-hub/kms/src/plugins/mod.rs
+++ confidential-data-hub/kms/src/plugins/mod.rs
@@ -3,12 +3,15 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-use crate::{Decrypter, Error, ProviderSettings, Result};
+use crate::{Decrypter, Error, Getter, ProviderSettings, Result};
 
 const IN_GUEST_DEFAULT_KEY_PATH: &str = "/run/confidential-containers/cdh/kms-credential";
 
 #[cfg(feature = "aliyun")]
 pub mod aliyun;
+
+#[cfg(feature = "kbs")]
+pub mod kbs;
 
 /// Create a new [`Decrypter`] by given provider name and [`ProviderSettings`]
 pub async fn new_decryptor(
@@ -20,6 +23,19 @@ pub async fn new_decryptor(
         "aliyun" => Ok(Box::new(
             aliyun::AliyunKmsClient::from_provider_settings(&_provider_settings).await?,
         ) as Box<dyn Decrypter>),
+        p => Err(Error::UnsupportedProvider(p.to_string())),
+    }
+}
+
+/// Create a new [`Getter`] by given provider name and [`ProviderSettings`]
+pub async fn new_getter(
+    provider: &str,
+    _provider_settings: ProviderSettings,
+) -> Result<Box<dyn Getter>> {
+    match provider {
+        #[cfg(feature = "kbs")]
+        "kbs" => Ok(Box::new(kbs::KbsClient::new().await?) as Box<dyn Getter>),
+        // TODO: Add sev-*-kbc series here
         p => Err(Error::UnsupportedProvider(p.to_string())),
     }
 }
