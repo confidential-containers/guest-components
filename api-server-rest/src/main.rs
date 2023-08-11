b6aa1ac22bfb2ae2b465ea009604c0477e22e255
Wang, Arron <arron.wang@intel.com>
2023-08-11 19:18:01 +0800

    rest_api: Add the router to dispatch url request
    Signed-off-by: Wang, Arron <arron.wang@intel.com>

--- api-server-rest/src/main.rs
+++ api-server-rest/src/main.rs
@@ -6,8 +6,13 @@
 use clap::Parser;
 use hyper::server::conn::AddrStream;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
+use hyper::Server;
 use std::net::SocketAddr;
+use std::sync::Arc;
+
+mod router;
+mod utils;
+use crate::router::Router;
 
 type GenericError = Box<dyn std::error::Error + Send + Sync>;
 type Result<T> = std::result::Result<T, GenericError>;
@@ -23,10 +28,6 @@ struct Args {
     bind: String,
 }
 
-async fn api_handler(_req: Request<Body>) -> Result<Response<Body>> {
-    Ok(Response::new(Body::empty()))
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
@@ -35,8 +36,19 @@ async fn main() -> Result<()> {
 
     let address: SocketAddr = args.bind.parse().expect("Failed to parse the address");
 
-    let api_service = make_service_fn(|_conn: &AddrStream| {
-        async move { Ok::<_, GenericError>(service_fn(api_handler)) }
+    let router = Router::new();
+    let router = Arc::new(tokio::sync::Mutex::new(router));
+
+    let api_service = make_service_fn(|conn: &AddrStream| {
+        let remote_addr = conn.remote_addr();
+        let local_router = router.clone();
+
+        async move {
+            Ok::<_, GenericError>(service_fn(move |req| {
+                let local_router = local_router.clone();
+                async move { local_router.lock().await.route(remote_addr, req).await }
+            }))
+        }
     });
 
     let server = Server::bind(&address).serve(api_service);
---
+++ api-server-rest/src/router.rs
@@ -0,0 +1,94 @@
+// Copyright (c) 2023 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+use anyhow::*;
+use async_trait::async_trait;
+use hyper::{header, Body, Request, Response, StatusCode};
+use std::collections::HashMap;
+use std::net::SocketAddr;
+
+use crate::utils::split_nth_slash;
+
+#[async_trait]
+pub trait ApiHandler: Send {
+    async fn handle_request(
+        &self,
+        _remote_addr: SocketAddr,
+        _resource_path: &str,
+        _req: Request<Body>,
+    ) -> Result<Response<Body>> {
+        Ok(Response::new(Body::empty()))
+    }
+
+    // Build octet-stream response for bytes data.
+    fn octet_stream_response(&self, data: Vec<u8>) -> Result<Response<Body>> {
+        Ok(Response::builder()
+            .status(StatusCode::OK)
+            .header(header::CONTENT_TYPE, "application/octet-stream")
+            .body(Body::from(data))?)
+    }
+
+    // Build json response.
+    fn json_response(&self, json: String) -> Result<Response<Body>> {
+        Ok(Response::builder()
+            .status(StatusCode::OK)
+            .header(header::CONTENT_TYPE, "application/json")
+            .body(Body::from(json))?)
+    }
+
+    // Build 403 Forbidden response.
+    fn forbidden(&self) -> Result<Response<Body>> {
+        Ok(Response::builder()
+            .status(StatusCode::FORBIDDEN)
+            .body(Body::from("Forbidden"))?)
+    }
+
+    // Build 404 Not Found response.
+    fn not_found(&self) -> Result<Response<Body>> {
+        Ok(Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::from("URL NOT FOUND"))?)
+    }
+
+    // Build 405 Method Not Allowed response.
+    fn not_allowed(&self) -> Result<Response<Body>> {
+        Ok(Response::builder()
+            .status(StatusCode::METHOD_NOT_ALLOWED)
+            .body(Body::from("Method Not Allowed"))?)
+    }
+}
+
+pub struct Router {
+    routes: HashMap<String, Box<dyn ApiHandler + Sync + Send>>,
+}
+
+impl Router {
+    pub fn new() -> Self {
+        Router {
+            routes: HashMap::new(),
+        }
+    }
+
+    pub fn register_route(&mut self, route: &str, handler: Box<dyn ApiHandler + Sync + Send>) {
+        self.routes.insert(route.to_string(), handler);
+    }
+
+    pub async fn route(
+        &mut self,
+        remote_addr: SocketAddr,
+        req: Request<Body>,
+    ) -> Result<Response<Body>> {
+        if let Some((root_path, url_path)) = split_nth_slash(req.uri().path(), 2) {
+            println!("root_path {}, url_path {}", root_path, url_path);
+            let local_url = url_path.to_string();
+            match self.routes.get(root_path) {
+                Some(handler) => return handler.handle_request(remote_addr, &local_url, req).await,
+                None => return Ok(Response::builder().status(404).body(Body::empty())?),
+            }
+        }
+
+        Ok(Response::builder().status(404).body(Body::empty())?)
+    }
+}
