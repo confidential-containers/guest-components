a9903d40d38b63f7c7ebd752f42aa76ec40a4b18
Ubuntu <ec2-user@1kubernetesVM000001.4wi25tg02ieu1bvlaiwkxdvrfb.bx.internal.cloudapp.net>
2023-10-18 12:27:59 +0000

    target to /run

--- src/snapshots/occlum/unionfs.rs
+++ src/snapshots/occlum/unionfs.rs
@@ -239,7 +239,7 @@ impl Snapshotter for Unionfs {
             mountpoint_c.as_c_str(),
             Some(fs_type_2.as_str()),
             flags,
-            Some("dir=/keys"),
+            Some("dir=/run/enclave-cc/containers/"),
         ).unwrap_or_else(|e| log::error!("mount failed: {}", e));
 
         // create environment for Occlum
