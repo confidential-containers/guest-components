e51f244bbf69d64a89df93f91b0b9ce277e13884
Linda Yu <linda.yu@intel.com>
2023-10-25 21:31:04 +0800

    cdh/storage: create tmp dir for oss
    Signed-off-by: Linda Yu <linda.yu@intel.com>

--- confidential-data-hub/storage/src/volume_type/alibaba_cloud_oss/oss.rs
+++ confidential-data-hub/storage/src/volume_type/alibaba_cloud_oss/oss.rs
@@ -6,6 +6,7 @@
 use base64::{engine::general_purpose::STANDARD, Engine};
 use secret::secret::Secret;
 use serde::{Deserialize, Serialize};
+use std::fs;
 use std::fs::File;
 use std::io::Write;
 use std::os::unix::fs::PermissionsExt;
@@ -114,6 +115,8 @@ impl Oss {
             .map(str::to_string)
             .collect();
         let s = if self.encrypted == "gocryptfs" {
+            fs::create_dir_all("/tmp/oss")
+                .map_err(|e| Error::FileError(format!("create dir failed: {e}")))?;
             "/tmp/oss/".to_string()
         } else {
             source.clone()
