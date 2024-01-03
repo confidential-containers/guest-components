8140458af08266c768a5c4556ab09ea24c9b7022
Jiale Zhang <zhangjiale@linux.alibaba.com>
2024-01-03 15:08:42 +0800

    AA: Extract GetToken logic as a crate
    Signed-off-by: Jiale Zhang <zhangjiale@linux.alibaba.com>

--- attestation-agent/lib/src/lib.rs
+++ attestation-agent/lib/src/lib.rs
@@ -14,10 +14,10 @@ use kbc::{AnnotationPacket, KbcCheckInfo, KbcInstance, KbcModuleList};
 use resource_uri::ResourceUri;
 use std::collections::HashMap;
 
-#[cfg(feature = "cc_kbc")]
-mod token;
-#[cfg(feature = "cc_kbc")]
-use token::get_kbs_token;
+mod config;
+
+#[cfg(any(feature = "cc_kbc", feature = "kbs_as"))]
+use token::GetToken;
 
 /// Attestation Agent (AA for short) is a rust library crate for attestation procedure
 /// in confidential containers. It provides kinds of service APIs that need to make
@@ -174,23 +174,22 @@ impl AttestationAPIs for AttestationAgent {
             .await
     }
 
+    #[allow(unused_variables)]
+    #[allow(unreachable_code)]
     async fn get_token(&mut self, _token_type: &str) -> Result<Vec<u8>> {
-        #[cfg(feature = "cc_kbc")]
-        {
-            let token = match _token_type {
-                "kbs" => get_kbs_token().await?,
-                typ => bail!("Unsupported token type {typ}"),
-            };
+        let token = match _token_type {
+            #[cfg(any(feature = "cc_kbc", feature = "kbs_as"))]
+            "kbs" => {
+                let kbs_host_url = config::get_host_url().await?;
+                let kbs_token = token::kbs::KbsTokenGetter::default()
+                    .get_token(kbs_host_url)
+                    .await?;
+                kbs_token
+            }
+            typ => bail!("Unsupported token type {typ}"),
+        };
 
-            Ok(token)
-        }
-
-        // TODO: remove the feature flags after refactoring AA. Currently, kbs_host_url
-        // is only set by user in aa_kbc_params when cc_kbc is enabled.
-        #[cfg(not(feature = "cc_kbc"))]
-        {
-            bail!("unimplemented!");
-        }
+        Ok(token)
     }
 
     /// Get TEE hardware signed evidence that includes the runtime data.
