93320b197db620bf9ba3bbaa540c64948dc49f39
Xynnn007 <xynnn@linux.alibaba.com>
2024-01-25 10:14:05 +0800

    Makefile: support to build components for all platforms
    This is to support a easy mode for users to build all guest components
    with all features enabled (including the attester plugins, etc)
    
    Also, fixed a bug in CDH when both kbs and sev features are enabled. We
    not will initialize sev when `aa_kbc_param` is set to `sev`.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- confidential-data-hub/hub/src/auth/mod.rs
+++ confidential-data-hub/hub/src/auth/mod.rs
@@ -15,7 +15,16 @@ impl Hub {
     pub(crate) async fn init(&mut self) -> Result<()> {
         #[cfg(feature = "sev")]
         {
-            Self::init_sev().await?;
+            use log::{info, warn};
+            match attestation_agent::aa_kbc_params::get_params().await {
+                Ok(aa_kbc_params) => {
+                    if aa_kbc_params.kbc() == "online_sev_kbc" {
+                        info!("online_sev_kbc used. Start to initialize sev.");
+                        Self::init_sev().await?;
+                    }
+                }
+                Err(e) => warn!("Get `aa_kbc_params` failed. Skip initialize sev. {e:?}"),
+            };
         }
 
         #[cfg(feature = "kbs")]
