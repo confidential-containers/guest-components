820bc4ff297d49d9a9f7332dfc64fb78cc508dad
Xynnn007 <xynnn@linux.alibaba.com>
2024-04-07 12:56:17 +0800

    AA: add built-in plugin information in -V
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- attestation-agent/attestation-agent/build.rs
+++ attestation-agent/attestation-agent/build.rs
@@ -34,5 +34,62 @@ fn main() -> std::io::Result<()> {
             .expect("Generate ttrpc protocol code failed.");
     }
 
+    #[cfg(feature = "bin")]
+    {
+        use std::env;
+        use std::fs::File;
+        use std::io::Write;
+        use std::path::Path;
+        use std::process::Command;
+
+        // generate an `intro` file that includes the feature information of the build
+        fn feature_list(features: Vec<&str>) -> String {
+            let enabled_features: Vec<&str> = features
+                .into_iter()
+                .filter(|&feature| env::var(format!("CARGO_FEATURE_{}", feature)).is_ok())
+                .collect();
+
+            enabled_features.join(", ")
+        }
+
+        let token_plugins = feature_list(vec!["KBS", "COCO_AS"]);
+        let attester = feature_list(vec![
+            "TDX_ATTESTER",
+            "SGX_ATTESTER",
+            "AZ_SNP_VTPM_ATTESTER",
+            "AZ_TDX_VTPM_ATTESTER",
+            "SNP_ATTESTER",
+        ]);
+
+        let out_dir = env::var("OUT_DIR").unwrap();
+        let dest_path = Path::new(&out_dir).join("version");
+        let mut f = File::create(dest_path).unwrap();
+        let git_commit_hash = Command::new("git")
+            .args(["rev-parse", "HEAD"])
+            .output()
+            .unwrap()
+            .stdout;
+
+        let git_commit_hash = String::from_utf8(git_commit_hash).unwrap();
+        let git_commit_hash = git_commit_hash.trim_end();
+
+        let git_status_output = match Command::new("git")
+            .args(["diff", "HEAD"])
+            .output()
+            .unwrap()
+            .stdout
+            .is_empty()
+        {
+            true => "",
+            false => "(dirty)",
+        };
+
+        writeln!(f, "\n\nCommit Hash: {git_commit_hash} {git_status_output}",).unwrap();
+
+        writeln!(f, "Supported Attesters: {}", attester).unwrap();
+
+        writeln!(f, "Token plugins: {}", token_plugins).unwrap();
+    }
+
     Ok(())
 }
