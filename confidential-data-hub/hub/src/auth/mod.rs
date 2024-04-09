9f0e3e2e91f0a131443091a593e4d4a5a1989440
Xynnn007 <xynnn@linux.alibaba.com>
2024-04-09 17:44:48 +0800

    CDH: move sev init into KMS plugins
    The initailization of sev should including setting a kernel module,
    mount a security fs and read the file content from the fs. In the old
    version before reading the fs, the kernel module has already been
    removed. This commit fixes this by move the logic into the
    initialization of online-sev-kbc.
    
    Also, the `credentials` items inside CDH config would also make sense to
    online-sev-kbc and offline-fs-kbc except for cc-kbc.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- confidential-data-hub/hub/src/auth/mod.rs
+++ confidential-data-hub/hub/src/auth/mod.rs
@@ -5,28 +5,11 @@
 
 use crate::{hub::Hub, Result};
 
-#[cfg(feature = "sev")]
-mod sev;
-
 #[cfg(feature = "kbs")]
 mod kbs;
 
 impl Hub {
     pub(crate) async fn init(&mut self) -> Result<()> {
-        #[cfg(feature = "sev")]
-        {
-            use log::{info, warn};
-            match attestation_agent::config::aa_kbc_params::get_params() {
-                Ok(aa_kbc_params) => {
-                    if aa_kbc_params.kbc() == "online_sev_kbc" {
-                        info!("online_sev_kbc used. Start to initialize sev.");
-                        Self::init_sev().await?;
-                    }
-                }
-                Err(e) => warn!("Get `aa_kbc_params` failed. Skip initialize sev. {e:?}"),
-            };
-        }
-
         #[cfg(feature = "kbs")]
         {
             self.init_kbs_resources().await?;
--- confidential-data-hub/kms/src/plugins/kbs/sev/client.rs
+++ confidential-data-hub/kms/src/plugins/kbs/sev/client.rs
@@ -9,9 +9,10 @@ use std::collections::HashMap;
 use async_trait::async_trait;
 use base64::{engine::general_purpose::STANDARD, Engine};
 use crypto::WrapType;
+use lazy_static::lazy_static;
 use resource_uri::ResourceUri;
 use serde::Deserialize;
-use tokio::fs;
+use tokio::{fs, sync::RwLock};
 use tonic::transport::Uri;
 use uuid::Uuid;
 use zeroize::Zeroizing;
@@ -24,20 +25,33 @@ use super::keybroker::{
 
 const KEYS_PATH: &str = "/sys/kernel/security/secrets/coco/1ee27366-0c87-43a6-af48-28543eaf7cb0";
 
+lazy_static! {
+    static ref ONLINE_SEV_KBC: RwLock<Option<RealKbc>> = RwLock::new(None);
+}
+
 #[derive(Deserialize, Clone)]
 struct Connection {
     client_id: Uuid,
     key: String,
 }
 
-pub struct OnlineSevKbc {
+struct RealKbc {
     client_id: Uuid,
     key: Vec<u8>,
     kbs_uri: Uri,
 }
 
-impl OnlineSevKbc {
-    pub async fn new(kbs_uri: &str) -> Result<Self> {
+impl RealKbc {
+    async fn new(kbs_uri: &str) -> Result<Self> {
+        sev::mount_security_fs().map_err(|e| {
+            Error::KbsClientError(format!("online-sev-kbc: mount security fs failed: {e}"))
+        })?;
+        let _secret_module = sev::SecretKernelModule::new().map_err(|e| {
+            Error::KbsClientError(format!(
+                "online-sev-kbc: create SecretKernelModule failed: {e}"
+            ))
+        })?;
+
         let connection_json = fs::read_to_string(KEYS_PATH)
             .await
             .map_err(|e| Error::KbsClientError(format!("online-sev-kbc: Read keys failed: {e}")))?;
@@ -58,11 +72,27 @@ impl OnlineSevKbc {
         let kbs_uri = format!("http://{kbs_uri}").parse::<Uri>().map_err(|e| {
             Error::KbsClientError(format!("online-sev-kbc: parse kbs uri failed: {e}"))
         })?;
-        Ok(Self {
+        let kbc = RealKbc {
             client_id: connection.client_id,
             key,
             kbs_uri,
-        })
+        };
+
+        Ok(kbc)
+    }
+}
+
+pub struct OnlineSevKbc {}
+
+impl OnlineSevKbc {
+    pub async fn new(kbs_uri: &str) -> Result<Self> {
+        if !ONLINE_SEV_KBC.read().await.is_none() {
+            let mut writer = ONLINE_SEV_KBC.write().await;
+            let kbc = RealKbc::new(kbs_uri).await?;
+            *writer = Some(kbc);
+        }
+
+        Ok(Self {})
     }
 
     async fn get_resource_from_kbs(
@@ -70,7 +100,9 @@ impl OnlineSevKbc {
         resource_uri: ResourceUri,
         secret_type: &str,
     ) -> Result<Vec<u8>> {
-        let channel = tonic::transport::Channel::builder(self.kbs_uri.clone()).connect_lazy();
+        let reader = ONLINE_SEV_KBC.read().await;
+        let kbc = reader.as_ref().expect("Must be initialized");
+        let channel = tonic::transport::Channel::builder(kbc.kbs_uri.clone()).connect_lazy();
         let mut client = KeyBrokerServiceClient::new(channel);
 
         let guid = Uuid::new_v4().as_hyphenated().to_string();
@@ -82,7 +114,7 @@ impl OnlineSevKbc {
         };
 
         let request = tonic::Request::new(OnlineSecretRequest {
-            client_id: self.client_id.as_hyphenated().to_string(),
+            client_id: kbc.client_id.as_hyphenated().to_string(),
             secret_requests: vec![secret_request],
         });
 
@@ -94,7 +126,7 @@ impl OnlineSevKbc {
             })?
             .into_inner();
         let decrypted_payload = crypto::decrypt(
-            Zeroizing::new(self.key.clone()),
+            Zeroizing::new(kbc.key.clone()),
             STANDARD.decode(response.payload).map_err(|e| {
                 Error::KbsClientError(format!(
                     "online-sev-kbc: base64 decode response.payload failed: {e}"
@@ -131,8 +163,10 @@ impl OnlineSevKbc {
 #[async_trait]
 impl Kbc for OnlineSevKbc {
     async fn get_resource(&mut self, rid: ResourceUri) -> Result<Vec<u8>> {
+        let reader = ONLINE_SEV_KBC.read().await;
+        let kbc = reader.as_ref().expect("Must be initialized");
         match &rid.r#type[..] {
-            "client-id" => Ok(self.client_id.hyphenated().to_string().into_bytes()),
+            "client-id" => Ok(kbc.client_id.hyphenated().to_string().into_bytes()),
             _ => self.get_resource_from_kbs(rid, "resource").await,
         }
     }
