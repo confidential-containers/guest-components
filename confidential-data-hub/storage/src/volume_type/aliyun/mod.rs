7388b5a4f80cdbb8e75099e7c058481316fdabf5
Xynnn007 <xynnn@linux.alibaba.com>
2024-05-09 11:57:49 +0800

    CDH/storage: fix aliyun mount
    This fixes unexpected deletion of tmp dir, which will cause the mount
    point empty.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- confidential-data-hub/storage/src/volume_type/aliyun/mod.rs
+++ confidential-data-hub/storage/src/volume_type/aliyun/mod.rs
@@ -10,9 +10,14 @@ use std::{collections::HashMap, os::unix::fs::PermissionsExt};
 
 use anyhow::Context;
 use async_trait::async_trait;
-use log::debug;
+use log::{debug, error};
+use rand::{distributions::Alphanumeric, Rng};
 use serde::{Deserialize, Serialize};
-use tokio::{fs, io::AsyncWriteExt, process::Command};
+use tokio::{
+    fs,
+    io::{AsyncReadExt, AsyncWriteExt},
+    process::Command,
+};
 
 use error::{AliyunError, Result};
 
@@ -69,6 +74,22 @@ async fn get_plaintext_secret(secret: &str) -> anyhow::Result<String> {
     }
 }
 
+async fn create_random_dir() -> anyhow::Result<String> {
+    const NAME_LENGTH: usize = 10;
+
+    let name: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(NAME_LENGTH)
+        .map(char::from)
+        .collect();
+
+    let path_name = format!("/tmp/{name}");
+    fs::create_dir_all(&path_name)
+        .await
+        .context("create /tmp dir")?;
+    Ok(path_name)
+}
+
 impl Oss {
     async fn real_mount(
         &self,
@@ -85,7 +106,7 @@ impl Oss {
         let plain_ak_id = get_plaintext_secret(&oss_parameter.ak_id).await?;
         let plain_ak_secret = get_plaintext_secret(&oss_parameter.ak_secret).await?;
 
-        // create temp directory to storage metadata for this mount operation
+        // create temp directory to store metadata for this mount operation
         let tempdir = tempfile::tempdir()?;
 
         // create ossfs passwd file
@@ -105,7 +126,8 @@ impl Oss {
                 .as_bytes(),
             )
             .await?;
-        ossfs_passwd.flush().await?;
+        ossfs_passwd.sync_all().await?;
+        drop(ossfs_passwd);
 
         // generate parameters for ossfs command
         let mut opts = oss_parameter
@@ -115,12 +137,11 @@ impl Oss {
             .collect();
 
         if oss_parameter.encrypted == "gocryptfs" {
-            let gocryptfs_dir = tempfile::tempdir()?;
+            let gocryptfs_dir = create_random_dir().await?;
 
-            let gocryptfs_dir_path = gocryptfs_dir.path().to_string_lossy().to_string();
             let mut parameters = vec![
                 format!("{}:{}", oss_parameter.bucket, oss_parameter.path),
-                gocryptfs_dir_path.clone(),
+                gocryptfs_dir.clone(),
                 format!("-ourl={}", oss_parameter.url),
                 format!("-opasswd_file={ossfs_passwd_path}"),
             ];
@@ -129,10 +150,21 @@ impl Oss {
             let mut oss = Command::new(OSSFS_BIN)
                 .args(parameters)
                 .spawn()
-                .map_err(|_| AliyunError::OssfsMountFailed)?;
+                .map_err(|e| {
+                    error!("oss cmd fork failed: {e}");
+                    AliyunError::OssfsMountFailed
+                })?;
             let oss_res = oss.wait().await?;
             if !oss_res.success() {
                 {
+                    let mut stderr = String::new();
+                    if let Some(mut err) = oss.stderr {
+                        err.read_to_string(&mut stderr).await?;
+                        error!("OSS mount failed with stderr: {stderr}");
+                    } else {
+                        error!("OSS mount failed");
+                    }
+
                     return Err(AliyunError::OssfsMountFailed);
                 }
             }
@@ -147,11 +179,12 @@ impl Oss {
             let mut gocryptfs_passwd = fs::File::create(&gocryptfs_passwd_path).await?;
 
             gocryptfs_passwd.write_all(plain_passwd.as_bytes()).await?;
-            gocryptfs_passwd.flush().await?;
+            gocryptfs_passwd.sync_all().await?;
+            drop(gocryptfs_passwd);
 
             // generate parameters for gocryptfs, and execute
             let parameters = vec![
-                gocryptfs_dir_path,
+                gocryptfs_dir,
                 mount_point.to_string(),
                 "-passfile".to_string(),
                 gocryptfs_passwd_path,
@@ -165,6 +198,14 @@ impl Oss {
             let gocryptfs_res = gocryptfs.wait().await?;
             if !gocryptfs_res.success() {
                 {
+                    let mut stderr = String::new();
+
+                    if let Some(mut err) = gocryptfs.stderr {
+                        err.read_to_string(&mut stderr).await?;
+                        error!("gocryptfs failed with stderr: {stderr}");
+                    } else {
+                        error!("gocryptfs failed");
+                    }
                     return Err(AliyunError::GocryptfsMountFailed);
                 }
             }
@@ -180,10 +221,20 @@ impl Oss {
             let mut oss = Command::new(OSSFS_BIN)
                 .args(parameters)
                 .spawn()
-                .map_err(|_| AliyunError::OssfsMountFailed)?;
+                .map_err(|e| {
+                    error!("oss cmd fork failed: {e}");
+                    AliyunError::OssfsMountFailed
+                })?;
             let oss_res = oss.wait().await?;
             if !oss_res.success() {
                 {
+                    let mut stderr = String::new();
+                    if let Some(mut err) = oss.stderr {
+                        err.read_to_string(&mut stderr).await?;
+                        error!("oss mount failed with stderr: {stderr}");
+                    } else {
+                        error!("oss mount failed");
+                    }
                     return Err(AliyunError::OssfsMountFailed);
                 }
             }
