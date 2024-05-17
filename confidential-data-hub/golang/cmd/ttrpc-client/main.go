f8360a62489dc19f20810fc0c35aa2284709c964
ChengyuZhu6 <chengyu.zhu@intel.com>
2024-05-17 20:05:40 +0800

    cdh:golang: build cdh go ttrpc client
    Support to build cdh go ttrpc client.
    
    Signed-off-by: ChengyuZhu6 <chengyu.zhu@intel.com>

---
+++ confidential-data-hub/golang/cmd/ttrpc-client/main.go
@@ -0,0 +1,44 @@
+// Copyright (c) 2024 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	cdhttrpcapi "github.com/confidential-containers/guest-components/confidential-data-hub/golang/pkg/ttrpc"
+)
+
+func main() {
+
+	vFlag := flag.String("v", "", "sealed secret value")
+	fFlag := flag.String("f", "", "sealed secret file")
+
+	flag.Parse()
+
+	c, err := cdhttrpcapi.CreateCDHTtrpcClient(cdhttrpcapi.CDHTtrpcSocket)
+	if err != nil {
+		fmt.Printf("failed to create cdh client %v", err)
+		os.Exit(1)
+	}
+	defer c.Close()
+	if *vFlag != "" {
+		unsealedValue, err := c.UnsealEnv(context.Background(), *vFlag)
+		if err != nil {
+			fmt.Printf("failed to get unsealed value! err = %v", err)
+			os.Exit(1)
+		}
+		fmt.Printf("unsealed value from env= %s", unsealedValue)
+	}
+	if *fFlag != "" {
+		unsealedValue, err := c.UnsealFile(context.Background(), *fFlag)
+		if err != nil {
+			fmt.Printf("failed to get unsealed value! err = %v", err)
+			os.Exit(1)
+		}
+		fmt.Printf("unsealed value from file = %s", unsealedValue)
+	}
+}
