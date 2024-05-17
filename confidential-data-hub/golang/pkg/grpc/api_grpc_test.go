1cbe7509ef22f09d7c7c2d47e4544551d0aa8a87
ChengyuZhu6 <chengyu.zhu@intel.com>
2024-05-17 19:51:09 +0800

    cdh:golang: unseal secret from file in cdh grpc client
    Support to unseal secret from file in cdh grpc client.
    
    Signed-off-by: ChengyuZhu6 <chengyu.zhu@intel.com>

--- confidential-data-hub/golang/pkg/grpc/api_grpc_test.go
+++ confidential-data-hub/golang/pkg/grpc/api_grpc_test.go
@@ -6,6 +6,8 @@ package api
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/confidential-containers/guest-components/confidential-data-hub/golang/pkg/grpc/mock"
@@ -42,4 +44,37 @@ func TestGrpcUnsealEnv(t *testing.T) {
 	resp, err := c.UnsealEnv(ctx, "sealed.111")
 	assert.Nil(err)
 	assert.Equal("unsealed-value:111", resp)
+}
+
+func TestGrpcUnsealFile(t *testing.T) {
+	assert := assert.New(t)
+
+	cdhMockServer := mock.CDHGrpcMockServer{}
+	err := cdhMockServer.Start(":8043")
+	assert.NoError(err)
+	defer cdhMockServer.Stop()
+
+	c, err := CreateCDHGrpcClient(CDHGrpcSocket)
+	assert.NoError(err)
+	assert.NotNil(c)
+	defer c.Close()
+
+	ctx := context.Background()
+
+	tempDir, err := os.MkdirTemp("", "test")
+	if err != nil {
+		t.Fatalf("Failed to create temp dir: %v", err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	sealedFilePath := filepath.Join(tempDir, "testfile")
+	unsealedContent := "unsealed content"
+	err = os.WriteFile(sealedFilePath, []byte("sealed."+unsealedContent), 0644)
+	if err != nil {
+		t.Fatalf("Failed to write sealed file: %v", err)
+	}
+
+	resp, err := c.UnsealFile(ctx, sealedFilePath)
+	assert.Nil(err)
+	assert.Equal("unsealed-value:unsealed content", resp)
 }
