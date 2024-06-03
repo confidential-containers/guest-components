474879a10d906c71f79d8d33d414c5a7cccba86a
Mikko Ylinen <mikko.ylinen@intel.com>
2024-06-03 15:25:35 +0300

    tdx-attester: update TD platform detection
    The attester crate pulls in DCAP 1.20 which only supports
    /dev/tdx_guest ioctls so the two other (legacy) character devices
    can be dropped.
    
    However, this triggers another issue: we can have /dev/tdx_guest
    ioctls (e.g., the RTMR extend) that are not part of Linux upstream
    so the recently added runtime_measurement_extend_available() won't
    work anymore. This change tries to mitigate that (for the time being)
    by making the simplification that "if TSM reports, no RTMRs".
    
    Signed-off-by: Mikko Ylinen <mikko.ylinen@intel.com>

--- attestation-agent/attester/src/tdx/mod.rs
+++ attestation-agent/attester/src/tdx/mod.rs
@@ -24,13 +24,7 @@ const CCEL_PATH: &str = "/sys/firmware/acpi/tables/data/CCEL";
 const RUNTIME_MEASUREMENT_RTMR_INDEX: u64 = 2;
 
 pub fn detect_platform() -> bool {
-    TsmReportPath::new(TsmReportProvider::Tdx).is_ok() || tdx_getquote_ioctl_is_available()
-}
-
-fn tdx_getquote_ioctl_is_available() -> bool {
-    Path::new("/dev/tdx-attest").exists()
-        || Path::new("/dev/tdx-guest").exists()
-        || Path::new("/dev/tdx_guest").exists()
+    TsmReportPath::new(TsmReportProvider::Tdx).is_ok() || Path::new("/dev/tdx_guest").exists()
 }
 
 fn get_quote_ioctl(report_data: &Vec<u8>) -> Result<Vec<u8>> {
@@ -47,16 +41,6 @@ fn get_quote_ioctl(report_data: &Vec<u8>) -> Result<Vec<u8>> {
             error_code
         )),
     }
-}
-
-// Return true if the TD environment can extend runtime measurement,
-// else false.
-fn runtime_measurement_extend_available() -> bool {
-    if Path::new("/dev/tdx_guest").exists() || Path::new("/sys/kernel/config/tsm/report").exists() {
-        return false;
-    }
-
-    true
 }
 
 #[derive(Serialize, Deserialize)]
@@ -112,7 +96,9 @@ impl Attester for TdxAttester {
         events: Vec<Vec<u8>>,
         _register_index: Option<u64>,
     ) -> Result<()> {
-        if !runtime_measurement_extend_available() {
+        // Test if the TD can extend RTRMs. The best guess at the moment is that if "TSM reports"
+        // is available, the TD runs Linux upstream kernel and is _currently_ not able to do it.
+        if Path::new("/sys/kernel/config/tsm/report").exists() {
             bail!("TDX Attester: Cannot extend runtime measurement on this system");
         }
 
