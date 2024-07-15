d7a75398dadea932ceb19c55cfb0f76ae6528d50
Xynnn007 <xynnn@linux.alibaba.com>
2024-07-15 15:17:26 +0800

    AA: add GetTeeType API
    As a separate daemon service running not only for CoCo but also broader
    cases, AA should provide an API for callers to get current platform
    information. For example, on IaaS scenarios, a service daemon would
    request current platform information.
    
    This new API is used for a caller to get current platform name.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- attestation-agent/kbs_protocol/src/token_provider/aa/attestation_agent_ttrpc.rs
+++ attestation-agent/kbs_protocol/src/token_provider/aa/attestation_agent_ttrpc.rs
@@ -56,6 +56,11 @@ impl AttestationAgentServiceClient {
         let mut cres = super::attestation_agent::UpdateConfigurationResponse::new();
         ::ttrpc::async_client_request!(self, ctx, req, "attestation_agent.AttestationAgentService", "UpdateConfiguration", cres);
     }
+
+    pub async fn get_tee_type(&self, ctx: ttrpc::context::Context, req: &super::attestation_agent::GetTeeTypeRequest) -> ::ttrpc::Result<super::attestation_agent::GetTeeTypeResponse> {
+        let mut cres = super::attestation_agent::GetTeeTypeResponse::new();
+        ::ttrpc::async_client_request!(self, ctx, req, "attestation_agent.AttestationAgentService", "GetTeeType", cres);
+    }
 }
 
 struct GetEvidenceMethod {
@@ -113,6 +118,17 @@ impl ::ttrpc::r#async::MethodHandler for UpdateConfigurationMethod {
     }
 }
 
+struct GetTeeTypeMethod {
+    service: Arc<Box<dyn AttestationAgentService + Send + Sync>>,
+}
+
+#[async_trait]
+impl ::ttrpc::r#async::MethodHandler for GetTeeTypeMethod {
+    async fn handler(&self, ctx: ::ttrpc::r#async::TtrpcContext, req: ::ttrpc::Request) -> ::ttrpc::Result<::ttrpc::Response> {
+        ::ttrpc::async_request_handler!(self, ctx, req, attestation_agent, GetTeeTypeRequest, get_tee_type);
+    }
+}
+
 #[async_trait]
 pub trait AttestationAgentService: Sync {
     async fn get_evidence(&self, _ctx: &::ttrpc::r#async::TtrpcContext, _: super::attestation_agent::GetEvidenceRequest) -> ::ttrpc::Result<super::attestation_agent::GetEvidenceResponse> {
@@ -129,6 +145,9 @@ pub trait AttestationAgentService: Sync {
     }
     async fn update_configuration(&self, _ctx: &::ttrpc::r#async::TtrpcContext, _: super::attestation_agent::UpdateConfigurationRequest) -> ::ttrpc::Result<super::attestation_agent::UpdateConfigurationResponse> {
         Err(::ttrpc::Error::RpcStatus(::ttrpc::get_status(::ttrpc::Code::NOT_FOUND, "/attestation_agent.AttestationAgentService/UpdateConfiguration is not supported".to_string())))
+    }
+    async fn get_tee_type(&self, _ctx: &::ttrpc::r#async::TtrpcContext, _: super::attestation_agent::GetTeeTypeRequest) -> ::ttrpc::Result<super::attestation_agent::GetTeeTypeResponse> {
+        Err(::ttrpc::Error::RpcStatus(::ttrpc::get_status(::ttrpc::Code::NOT_FOUND, "/attestation_agent.AttestationAgentService/GetTeeType is not supported".to_string())))
     }
 }
 
@@ -151,6 +170,9 @@ pub fn create_attestation_agent_service(service: Arc<Box<dyn AttestationAgentSer
 
     methods.insert("UpdateConfiguration".to_string(),
                     Box::new(UpdateConfigurationMethod{service: service.clone()}) as Box<dyn ::ttrpc::r#async::MethodHandler + Send + Sync>);
+
+    methods.insert("GetTeeType".to_string(),
+                    Box::new(GetTeeTypeMethod{service: service.clone()}) as Box<dyn ::ttrpc::r#async::MethodHandler + Send + Sync>);
 
     ret.insert("attestation_agent.AttestationAgentService".to_string(), ::ttrpc::r#async::Service{ methods, streams });
     ret
