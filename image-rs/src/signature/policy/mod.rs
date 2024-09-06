d4a5762c94e4a4f867abea45cdba65672309bc4d
Xynnn007 <xynnn@linux.alibaba.com>
2024-09-06 17:10:36 +0800

    image-rs: refactor signature module
    Refactor signature module from function way to object oriented. Also
    changed the logic a little to avoid duplicated get resource operation.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- image-rs/src/signature/policy/mod.rs
+++ image-rs/src/signature/policy/mod.rs
@@ -3,19 +3,76 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-use anyhow::{bail, Result};
-use oci_client::secrets::RegistryAuth;
-use serde::Deserialize;
+use ref_match::PolicyReqMatchType;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use strum_macros::{Display, EnumString};
 
 use self::policy_requirement::PolicyReqType;
 
 use super::image;
-use super::mechanism::SignScheme;
 
 pub mod policy_requirement;
 pub mod ref_match;
+
+#[cfg(feature = "signature-cosign")]
+pub mod cosign;
+
+#[cfg(feature = "signature-simple")]
+pub mod simple;
+
+#[cfg(feature = "signature-simple")]
+pub use simple::sigstore::SigstoreConfig;
+
+#[derive(Deserialize, Debug, Eq, PartialEq, Serialize, Default)]
+pub struct CosignParameters {
+    // KeyPath is a pathname to a local file containing the trusted key(s).
+    // Exactly one of KeyPath and KeyData can be specified.
+    //
+    // This field is optional.
+    #[serde(rename = "keyPath")]
+    pub key_path: Option<String>,
+    // KeyData contains the trusted key(s), base64-encoded.
+    // Exactly one of KeyPath and KeyData can be specified.
+    //
+    // This field is optional.
+    #[serde(rename = "keyData")]
+    pub key_data: Option<String>,
+
+    // SignedIdentity specifies what image identity the signature must be claiming about the image.
+    // Defaults to "match-exact" if not specified.
+    //
+    // This field is optional.
+    #[serde(default, rename = "signedIdentity")]
+    pub signed_identity: Option<PolicyReqMatchType>,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq, Serialize, Default)]
+pub struct SimpleParameters {
+    // KeyType specifies what kind of the public key to verify the signatures.
+    #[serde(rename = "keyType")]
+    pub key_type: String,
+
+    // KeyPath is a pathname to a local file containing the trusted key(s).
+    // Exactly one of KeyPath and KeyData can be specified.
+    //
+    // This field is optional.
+    #[serde(rename = "keyPath")]
+    pub key_path: Option<String>,
+    // KeyData contains the trusted key(s), base64-encoded.
+    // Exactly one of KeyPath and KeyData can be specified.
+    //
+    // This field is optional.
+    #[serde(rename = "keyData")]
+    pub key_data: Option<String>,
+
+    // SignedIdentity specifies what image identity the signature must be claiming about the image.
+    // Defaults to "match-exact" if not specified.
+    //
+    // This field is optional.
+    #[serde(default, rename = "signedIdentity")]
+    pub signed_identity: Option<PolicyReqMatchType>,
+}
 
 #[derive(EnumString, Display, Debug, PartialEq, Eq)]
 pub enum ErrorInfo {
@@ -29,12 +86,12 @@ pub enum ErrorInfo {
     UnknownScheme,
 }
 
-// Policy defines requirements for considering a signature, or an image, valid.
-// The spec of it is defined in https://github.com/containers/image/blob/main/docs/containers-policy.json.5.md
+/// Policy defines requirements for considering a signature, or an image, valid.
+/// The spec of it is defined in https://github.com/containers/image/blob/main/docs/containers-policy.json.5.md
 #[derive(Deserialize)]
 pub struct Policy {
-    // `default` applies to any image which does not have a matching policy in Transports.
-    // Note that this can happen even if a matching `PolicyTransportScopes` exists in `transports`.
+    /// `default` applies to any image which does not have a matching policy in Transports.
+    /// Note that this can happen even if a matching `PolicyTransportScopes` exists in `transports`.
     default: Vec<PolicyReqType>,
     transports: HashMap<String, PolicyTransportScopes>,
 }
@@ -42,63 +99,33 @@ pub struct Policy {
 pub type PolicyTransportScopes = HashMap<String, Vec<PolicyReqType>>;
 
 impl Policy {
-    // Returns Ok(()) if the requirement allows running an image.
-    // WARNING: This validates signatures and the manifest, but does not download or validate the
-    // layers. Users must validate that the layers match their expected digests.
-    pub async fn is_image_allowed(
-        &mut self,
-        mut image: image::Image,
-        auth: &RegistryAuth,
-    ) -> Result<()> {
-        // Get the policy set that matches the image.
-        let reqs = self.requirements_for_image(&image);
-        if reqs.is_empty() {
-            bail!("List of verification policy requirements must not be empty");
-        }
-
-        // The image must meet the requirements of each policy in the policy set.
-        for req in reqs.iter() {
-            req.allows_image(&mut image, auth).await?;
-        }
-
-        Ok(())
-    }
-
-    // Get the set of signature schemes that need to be verified of the image.
-    pub fn signature_schemes(&mut self, image: &image::Image) -> Vec<&mut dyn SignScheme> {
-        self.requirements_for_image(image)
-            .iter_mut()
-            .filter_map(|req| req.try_into_sign_scheme())
-            .collect()
-    }
-
     // selects the appropriate requirements for the image from Policy.
-    fn requirements_for_image(&mut self, image: &image::Image) -> &mut Vec<PolicyReqType> {
+    pub(crate) fn requirements_for_image(&self, image: &image::Image) -> &Vec<PolicyReqType> {
         // Get transport name of the image
         let transport_name = image.transport_name();
 
-        if let Some(transport_scopes) = self.transports.get_mut(&transport_name) {
+        if let Some(transport_scopes) = self.transports.get(&transport_name) {
             // Look for a full match.
             let identity = image.reference.whole();
             if transport_scopes.contains_key(&identity) {
                 return transport_scopes
-                    .get_mut(&identity)
+                    .get(&identity)
                     .expect("Unexpected contains");
             }
 
             // Look for a match of the possible parent namespaces.
             for name in image::get_image_namespaces(&image.reference).iter() {
                 if transport_scopes.contains_key(name) {
-                    return transport_scopes.get_mut(name).expect("Unexpected contains");
+                    return transport_scopes.get(name).expect("Unexpected contains");
                 }
             }
 
             // Look for a default match for the transport.
-            if let Some(reqs) = transport_scopes.get_mut("") {
+            if let Some(reqs) = transport_scopes.get("") {
                 return reqs;
             }
         }
 
-        &mut self.default
+        &self.default
     }
 }
