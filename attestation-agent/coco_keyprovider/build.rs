b2a78f6dd9bd15ed91acd055218aa835dee91467
Xynnn007 <xynnn@linux.alibaba.com>
2024-12-17 09:43:20 +0800

    chore(deps): update shadow_rs to v0.37.0
    Close #847
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- attestation-agent/coco_keyprovider/build.rs
+++ attestation-agent/coco_keyprovider/build.rs
@@ -3,7 +3,12 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+use shadow_rs::{BuildPattern, ShadowBuilder};
+
 fn main() -> shadow_rs::SdResult<()> {
     tonic_build::compile_protos("../protos/keyprovider.proto")?;
-    shadow_rs::new()
+    ShadowBuilder::builder()
+        .build_pattern(BuildPattern::RealTime)
+        .build()?;
+    Ok(())
 }
