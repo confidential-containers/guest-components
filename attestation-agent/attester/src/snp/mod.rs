9ce8c4d8e43dbbad19caf646780f6d91411808b6
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-01-27 20:36:49 +0100

    derived_key: fix function (typo)

--- attestation-agent/attester/src/snp/mod.rs
+++ attestation-agent/attester/src/snp/mod.rs
@@ -89,7 +89,7 @@ impl Attester for SnpAttester {
 
         let derived_key = firmware
             .get_derived_key(None, request)
-            .unwrap();
+            .unwrap()
             .context("Failed to get derived key")?;
 
         Ok(derived_key.to_vec())
