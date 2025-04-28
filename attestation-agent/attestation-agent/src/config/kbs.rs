3d6189b1d3b54b56a4861e94d529fc868d6e91b2
Xynnn007 <xynnn@linux.alibaba.com>
2025-04-28 17:43:19 +0800

    AA: fix configuration file
    Fixes scenarios where the configuration file is set as
    ```
    [token_config]
    ```
    or directly an empty file
    ```
    
    ```
    
    In both scenarios, a default value that no token configs are set will be
    used.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- attestation-agent/attestation-agent/src/config/kbs.rs
+++ attestation-agent/attestation-agent/src/config/kbs.rs
@@ -3,12 +3,12 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use serde::Deserialize;
 
 use super::aa_kbc_params::AaKbcParams;
 
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct KbsConfig {
     /// URL Address of KBS.
     pub url: String,
@@ -18,8 +18,14 @@ pub struct KbsConfig {
 }
 
 impl KbsConfig {
+    /// This function will try to read kbc and url from aa_kbc_params from env and kernel commandline.
+    /// If not given, or the kbc is not cc_kbc, it will return an error.
+    /// Because only cc_kbc will set kbs uri.
     pub fn new() -> Result<Self> {
         let aa_kbc_params = AaKbcParams::new()?;
+        if aa_kbc_params.kbc != "cc_kbc" {
+            bail!("specified aa_kbc_params.kbc is not kbs");
+        }
         Ok(Self {
             url: aa_kbc_params.uri,
             cert: None,
