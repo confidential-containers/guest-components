069a6ea010f44b759478b5cdc191d50416cb7deb
DGonzalezVillal <Diego.GonzalezVillalobos@amd.com>
2025-05-01 15:46:38 +0000

    Update SNP Attester
    Updated the SNP Attester to use version 6.0.0 of the VIRTEE SEV crate.
    Minor code adjustments were made to accommodate the library's new report-handling logic, but the overall functionality remains unchanged.
    
    Signed-off-by: DGonzalezVillal <Diego.GonzalezVillalobos@amd.com>

--- attestation-agent/attester/src/snp/hostdata.rs
+++ attestation-agent/attester/src/snp/hostdata.rs
@@ -4,6 +4,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+use sev::firmware::guest::{AttestationReport, Firmware};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -16,9 +17,9 @@ pub enum GetHostDataError {
 }
 
 pub fn get_snp_host_data() -> Result<[u8; 32], GetHostDataError> {
-    let mut firmware = sev::firmware::guest::Firmware::open()?;
+    let mut firmware = Firmware::open()?;
     let report_data: [u8; 64] = [0; 64];
-    let report = firmware.get_report(None, Some(report_data), Some(0))?;
-
-    Ok(report.host_data)
+    let report_bytes = firmware.get_report(None, Some(report_data), Some(0))?;
+    let report = AttestationReport::from_bytes(&report_bytes)?;
+    Ok(*report.host_data)
 }
