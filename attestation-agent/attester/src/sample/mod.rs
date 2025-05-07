dc47c54138c785bc53684afcfda7063df88abd1d
Tobin Feldman-Fitzthum <tobin@ibm.com>
2025-05-07 11:15:59 -0500

    attester: use json return type for verifiers
    This commit isn't stricly necessary for multi-device support, but it
    cleans up the interface for the attesters.
    
    Currently, attesters return a string, but every attester uses serde_json
    to do this. Instead, let's return a serde_json::Value (which we have a
    type alias for). This should save us a layer of encapsulation and makes
    things a little more explicit.
    
    Signed-off-by: Tobin Feldman-Fitzthum <tobin@ibm.com>

--- attestation-agent/attester/src/sample/mod.rs
+++ attestation-agent/attester/src/sample/mod.rs
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-use super::Attester;
+use super::{Attester, TeeEvidence};
 use anyhow::*;
 use base64::Engine;
 use serde::{Deserialize, Serialize};
@@ -25,12 +25,12 @@ pub struct SampleAttester {}
 
 #[async_trait::async_trait]
 impl Attester for SampleAttester {
-    async fn get_evidence(&self, report_data: Vec<u8>) -> Result<String> {
+    async fn get_evidence(&self, report_data: Vec<u8>) -> Result<TeeEvidence> {
         let evidence = SampleQuote {
             svn: "1".to_string(),
             report_data: base64::engine::general_purpose::STANDARD.encode(report_data),
         };
 
-        serde_json::to_string(&evidence).context("Serialize sample evidence failed")
+        serde_json::to_value(&evidence).context("Serialize sample evidence failed")
     }
 }
