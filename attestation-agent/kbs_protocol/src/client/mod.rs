fitzthum/image-rs

kbs_protocol: fix ClientTee enum
Fixup the spelling of the enum variants.

Signed-off-by: Tobin Feldman-Fitzthum <tobin@ibm.com>

attestation-agent/kbs_protocol/src/client/mod.rs
@@ -24,8 +24,8 @@ use kbs_types::Tee;
 use crate::{keypair::TeeKeyPair, token_provider::Token};
 
 pub(crate) enum ClientTee {
-    Unitialized,
-    _Initializated(Tee),
+    Uninitialized,
+    Initialized(Tee),
 }
 
 /// This Client is used to connect to the remote KBS.

attestation-agent/kbs_protocol/src/client/rcar_client.rs
@@ -144,12 +144,12 @@ impl KbsClient<Box<dyn EvidenceProvider>> {
         let auth_endpoint = format!("{}/{KBS_PREFIX}/auth", self.kbs_host_url);
 
         let tee = match &self._tee {
-            ClientTee::Unitialized => {
+            ClientTee::Uninitialized => {
                 let tee = self.provider.get_tee_type().await?;
-                self._tee = ClientTee::_Initializated(tee);
+                self._tee = ClientTee::Initialized(tee);
                 tee
             }
-            ClientTee::_Initializated(tee) => *tee,
+            ClientTee::Initialized(tee) => *tee,
         };
 
         let request = build_request(tee).await;

confidential-containers/guest-components

attestation-agent/kbs_protocol/src/client/mod.rs
@@ -24,8 +24,8 @@ use kbs_types::Tee;
 use crate::{keypair::TeeKeyPair, token_provider::Token};
 
 pub(crate) enum ClientTee {
-    Unitialized,
-    _Initializated(Tee),
+    Uninitialized,
+    _Initialized(Tee),
 }
 
 /// This Client is used to connect to the remote KBS.

attestation-agent/kbs_protocol/src/client/rcar_client.rs
@@ -144,12 +144,12 @@ impl KbsClient<Box<dyn EvidenceProvider>> {
         let auth_endpoint = format!("{}/{KBS_PREFIX}/auth", self.kbs_host_url);
 
         let tee = match &self._tee {
-            ClientTee::Unitialized => {
+            ClientTee::Uninitialized => {
                 let tee = self.provider.get_tee_type().await?;
-                self._tee = ClientTee::_Initializated(tee);
+                self._tee = ClientTee::_Initialized(tee);
                 tee
             }
-            ClientTee::_Initializated(tee) => *tee,
+            ClientTee::_Initialized(tee) => *tee,
         };
 
         let request = build_request(tee).await;