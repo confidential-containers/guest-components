ed3497f25761b899d259e29bf3cbc725905ad664
Tobin Feldman-Fitzthum <tfeldmanfitz@nvidia.com>
2026-01-29 12:00:52 -0800

    kbs_protocol: add plugin parameter to get_resource
    Rather than overload the ResourceURI to include information about the
    plugin, let's add a parameter to get_resource that explicitly specifies
    whcih plugin, and therefore which Trustee endpoint, to use.
    
    Add the parameter to get resource (rather than the client builder)
    because different resources can come from different plugins. In the CDH
    we don't construct a new client for every request, so we should allow
    setting the plugin per request.
    
    Signed-off-by: Tobin Feldman-Fitzthum <tfeldmanfitz@nvidia.com>

--- attestation-agent/kbs_protocol/src/api.rs
+++ attestation-agent/kbs_protocol/src/api.rs
@@ -9,5 +9,5 @@ pub use resource_uri::ResourceUri;
 
 #[async_trait]
 pub trait KbsClientCapabilities {
-    async fn get_resource(&mut self, resource_uri: ResourceUri) -> Result<Vec<u8>>;
+    async fn get_resource(&mut self, resource_uri: ResourceUri, plugin: String) -> Result<Vec<u8>>;
 }
