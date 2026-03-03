7525fb3bc3e1c557fa8bf7ad3b7403ea2e4f1a18
Tobin Feldman-Fitzthum <tfeldmanfitz@nvidia.com>
2026-03-03 06:35:39 -0800

    nvidia: replace NVML bindings with NVAT bindings
    NVAT is the official NVIDIA attestation SDK. It now has Rust bindings.
    Let's use these instead of the unmaintained NVML bindings.
    
    NVAT also has support for attesting nvswitches, which are outside of the
    scope of NVAT.
    
    That said, NVAT does not have an interface to set the device to ready,
    so we will delegate that to NVRC. NVAT does check that the device is in
    CC mode, though, so we no longer have to do that ourselves.
    
    Signed-off-by: Tobin Feldman-Fitzthum <tfeldmanfitz@nvidia.com>

--- attestation-agent/attester/src/nvidia/mod.rs
+++ attestation-agent/attester/src/nvidia/mod.rs
@@ -4,32 +4,40 @@
 //
 
 use super::{Attester, TeeEvidence};
-use anyhow::{bail, Context, Result};
-use base64::{engine::general_purpose::STANDARD, Engine};
-use nvml_wrapper::{enums::device::DeviceArchitecture, Nvml};
-use serde::Serialize;
+use anyhow::{Context, Result};
+use nv_attestation_sdk::{GpuEvidenceSource, Nonce, NvatSdk, SdkOptions, SwitchEvidenceSource};
+use serde::{Deserialize, Serialize};
+use strum::{Display, EnumString};
+use tracing::{info, warn};
 
 const NVIDIA_NONCE_SIZE: usize = 32;
 
+#[derive(Debug, Deserialize, Display, EnumString, PartialEq, Serialize)]
+#[strum(ascii_case_insensitive)]
+enum Architecture {
+    #[serde(alias = "BLACKWELL")]
+    Blackwell,
+    #[serde(alias = "HOPPER")]
+    Hopper,
+    LS10,
+}
+
 pub fn detect_platform() -> bool {
-    // Return true iff one GPU is found and it has CC mode set.
-    match Nvml::init() {
-        Ok(nvml) => {
-            nvml.device_count().is_ok_and(|count| count == 1)
-                && nvml
-                    .device_by_index(0)
-                    .is_ok_and(|device| device.is_cc_enabled().unwrap_or_default())
+    match get_device_evidence(None) {
+        Ok(ev) => !ev.is_empty(),
+        Err(e) => {
+            warn!("NVIDIA device detection failed due to: {}", e.to_string());
+            false
         }
-        Err(_) => false,
     }
 }
 
 /// NRAS knows about "switch" and "gpu" but the expected evidence
 /// content is the same. nvidia-attester can compose a list of
 /// all CC enabled nvml/nscq devices using this evidence struct.
-#[derive(Serialize)]
+#[derive(Deserialize, Serialize)]
 struct NvDeviceReportAndCert {
-    arch: DeviceArchitecture,
+    arch: Architecture,
     uuid: String,
     evidence: String,
     certificate: String,
@@ -48,60 +56,13 @@ impl Attester for NvAttester {
     /// Generate evidence for the NVIDIA devices. A 32 byte nonce is taken from the first 32
     /// report_data bytes. report_data shorter than 32 bytes is zero padded.
     async fn get_evidence(&self, mut report_data: Vec<u8>) -> Result<TeeEvidence> {
-        let nvml = Nvml::init()?;
-        let devices = nvml.device_count()?;
-
-        let mut device_evidence_list = vec![];
-
         if report_data.len() < NVIDIA_NONCE_SIZE {
             report_data.resize(NVIDIA_NONCE_SIZE, 0);
         }
 
         let nonce: [u8; NVIDIA_NONCE_SIZE] = report_data[0..NVIDIA_NONCE_SIZE].try_into()?;
 
-        for index in 0..devices {
-            let device = nvml.device_by_index(index)?;
-
-            let report = device
-                .confidential_compute_gpu_attestation_report(nonce)
-                .context("Failed to get attestation report for device {index}")?;
-
-            let certificate = device
-                .confidential_compute_gpu_certificate()
-                .context("Failed to get certificate for device {index}")?;
-
-            let dev_arch = device
-                .architecture()
-                .context("Failed to get architecture for device {index}")?;
-
-            let dev_uuid = device
-                .uuid()
-                .context("Failed to get UUID for device {index}")?;
-
-            let evidence = &report.attestation_report[..report.attestation_report_size as usize];
-            let cert_chain = &certificate.attestation_cert_chain
-                [..certificate.attestation_cert_chain_size as usize];
-
-            device_evidence_list.push(NvDeviceReportAndCert {
-                arch: dev_arch,
-                uuid: dev_uuid,
-                evidence: STANDARD.encode(evidence),
-                certificate: STANDARD.encode(cert_chain),
-            });
-
-            device
-                .set_confidential_compute_state(true)
-                .context("Failed to set device {index} to ready state")?;
-
-            // Run final sanity check to ensure the device confidential computing mode is enabled,
-            // it's in a production environment, and accepting client requests.
-            if !device
-                .check_confidential_compute_status()
-                .is_ok_and(|status| status)
-            {
-                bail!("NVIDIA attester: device {index} CC status check failed")
-            }
-        }
+        let device_evidence_list = get_device_evidence(Some(nonce))?;
 
         let full_evidence = NvDeviceEvidence {
             device_evidence_list,
@@ -109,4 +70,35 @@ impl Attester for NvAttester {
 
         serde_json::to_value(&full_evidence).context("Serialize NVIDIA evidence failed")
     }
+}
+
+/// Internal helper for getting evidence from NVIDIA devices.
+fn get_device_evidence(report_data: Option<[u8; 32]>) -> Result<Vec<NvDeviceReportAndCert>> {
+    let opts = SdkOptions::new()?;
+    let _sdk = NvatSdk::init(opts)?;
+
+    let nonce = match report_data {
+        Some(data_vec) => Nonce::from_hex(&hex::encode(data_vec))?,
+        None => Nonce::generate(32)?,
+    };
+
+    let gpu_source = GpuEvidenceSource::from_nvml()?;
+    let gpu_evidence = gpu_source.collect(&nonce)?;
+
+    let switch_source = SwitchEvidenceSource::from_nscq()?;
+    let switch_evidence = switch_source.collect(&nonce)?;
+
+    if gpu_evidence.is_empty() && switch_evidence.is_empty() {
+        info!("No NVIDIA GPUs or NVSwitches found.");
+        return Ok(vec![]);
+    }
+
+    let mut gpu_evidence: Vec<NvDeviceReportAndCert> =
+        serde_json::from_str(&gpu_evidence.to_json()?)?;
+    let switch_evidence: Vec<NvDeviceReportAndCert> =
+        serde_json::from_str(&switch_evidence.to_json()?)?;
+
+    gpu_evidence.extend(switch_evidence);
+
+    Ok(gpu_evidence)
 }
