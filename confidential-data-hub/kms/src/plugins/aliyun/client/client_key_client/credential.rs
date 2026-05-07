28aec33cff89ed49784ce34a3f668f82b26e5c76
Xynnn007 <xynnn@linux.alibaba.com>
2026-05-07 11:42:06 +0800

    deps/kms: replace p12, yasna and ring with openssl
    In history, we use ring dependency for static building, but now openssl
    supports static building. Thus we replace ring, yasna and p12 crate
    with openssl.
    
    Another benefit is that p12 is a very old crate that lacks maintaince.
    Switching to openssl would provide security and fixes.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- confidential-data-hub/kms/src/plugins/aliyun/client/client_key_client/credential.rs
+++ confidential-data-hub/kms/src/plugins/aliyun/client/client_key_client/credential.rs
@@ -7,11 +7,12 @@
 
 use anyhow::*;
 use base64::{engine::general_purpose::STANDARD, Engine};
-use p12::{CertBag, ContentInfo, MacData, SafeBagKind, PFX};
-use ring::{rand::SystemRandom, rsa::KeyPair, signature::RSA_PKCS1_SHA256};
+use openssl::hash::MessageDigest;
+use openssl::pkcs12::Pkcs12;
+use openssl::pkey::PKey;
+use openssl::provider::Provider;
+use openssl::sign::Signer;
 use serde::Deserialize;
-use tracing::debug;
-use yasna::ASN1Result;
 
 #[derive(Clone, Debug)]
 pub(crate) struct CredentialClientKey {
@@ -42,72 +43,30 @@ impl CredentialClientKey {
     }
 
     pub(crate) fn sign(&self, str_to_sign: &str) -> Result<String> {
-        let mut signature = Vec::new();
-        let private_key = KeyPair::from_pkcs8(&self.private_key)
+        let pkey = PKey::private_key_from_pkcs8(&self.private_key)
             .map_err(|_| anyhow!("read RSA private key failed"))?;
-        signature.resize(private_key.public().modulus_len(), 0);
-        private_key
-            .sign(
-                &RSA_PKCS1_SHA256,
-                &SystemRandom::new(),
-                str_to_sign.as_bytes(),
-                &mut signature,
-            )
+        let mut signer =
+            Signer::new(MessageDigest::sha256(), &pkey).map_err(|_| anyhow!("create signer"))?;
+        let signature = signer
+            .sign_oneshot_to_vec(str_to_sign.as_bytes())
             .map_err(|_| anyhow!("signing failed"))?;
 
         Ok(STANDARD.encode(signature))
     }
 
     pub(crate) fn parse_private_key(private_key_data: String, password: String) -> Result<Vec<u8>> {
-        let private_key_der = STANDARD.decode(private_key_data.as_bytes())?;
-        let pfx = yasna::parse_der(&private_key_der, |r| -> ASN1Result<PFX> {
-            r.read_sequence(|r| {
-                let version = r.next().read_u8()?;
-                let auth_safe = ContentInfo::parse(r.next())?;
-                let mac_data = r.read_optional(MacData::parse).ok().flatten();
-                std::result::Result::Ok(PFX {
-                    version,
-                    auth_safe,
-                    mac_data,
-                })
-            })
-        })?;
+        let pkcs12_der = STANDARD.decode(private_key_data.as_bytes())?;
+        let pkcs12 = Pkcs12::from_der(&pkcs12_der).context("parse PKCS#12 DER failed")?;
+        let _legacy_provider = Provider::try_load(None, "legacy", true).ok();
+        let parsed = pkcs12
+            .parse2(&password)
+            .context("decrypt PKCS#12 with password failed")?;
+        let pkey = parsed
+            .pkey
+            .ok_or_else(|| anyhow!("missing private key in PKCS#12"))?;
 
-        let bags = pfx.bags(&password)?;
-        for bag in &bags {
-            match &bag.bag {
-                SafeBagKind::Pkcs8ShroudedKeyBag(k) => {
-                    let pass = Self::bmp_string(&password);
-                    let private_key = k
-                        .encryption_algorithm
-                        .decrypt_pbe(&k.encrypted_data, &pass)
-                        .ok_or(anyhow!("decrypt pbe failed"))?;
-                    return Ok(private_key);
-                }
-                SafeBagKind::CertBag(e) => match e {
-                    CertBag::X509(x) => {
-                        debug!("parse aliyun pkcs12 credential X509: {}", hex::encode(x))
-                    }
-                    CertBag::SDSI(s) => debug!("parse aliyun pkcs12 credential SDSI: {s:?}"),
-                },
-                _ => continue,
-            }
-        }
-
-        Err(anyhow!("no private key found!"))
-    }
-
-    fn bmp_string(s: &str) -> Vec<u8> {
-        let utf16: Vec<u16> = s.encode_utf16().collect();
-
-        let mut bytes = Vec::with_capacity(utf16.len() * 2 + 2);
-        for c in utf16 {
-            bytes.push((c / 256) as u8);
-            bytes.push((c % 256) as u8);
-        }
-        bytes.push(0x00);
-        bytes.push(0x00);
-        bytes
+        pkey.private_key_to_pkcs8()
+            .context("export PKCS#8 private key failed")
     }
 }
 
