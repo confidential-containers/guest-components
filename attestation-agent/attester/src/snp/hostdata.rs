feb973709d544ddde951f8b6df5750b5afe522c9
CJ Maxwell <christopher.maxwell@amd.com>
2026-06-30 19:48:21 +0000

    prefer ConfigTSM path for generating attestation report in `get_snp_host_data()` function
    Fallback to using legacy `sev::firmware::guest::Firmware` if configtsm not supported
    
    Signed-off-by: CJ Maxwell <christopher.maxwell@amd.com>

--- attestation-agent/attester/src/snp/hostdata.rs
+++ attestation-agent/attester/src/snp/hostdata.rs
@@ -4,8 +4,12 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+use std::path::Path;
+
 use sev::firmware::guest::{AttestationReport, Firmware};
 use thiserror::Error;
+
+use crate::tsm_report::{TSM_REPORT_PATH, TsmReportData, TsmReportError, TsmReportPath, TsmReportProvider};
 
 #[derive(Error, Debug)]
 pub enum GetHostDataError {
@@ -14,12 +18,31 @@ pub enum GetHostDataError {
 
     #[error("Get report failed: {0}")]
     GetReportError(#[from] sev::error::UserApiError),
+
+    #[error("Get TSM report failed: {0}")]
+    GetTsmReportError(#[from] TsmReportError),
 }
 
 pub fn get_snp_host_data() -> Result<[u8; 32], GetHostDataError> {
-    let mut firmware = Firmware::open()?;
     let report_data: [u8; 64] = [0; 64];
-    let report_bytes = firmware.get_report(None, Some(report_data), Some(0))?;
+    let report_bytes: Vec<u8>;
+
+    if Path::new(TSM_REPORT_PATH).exists() {
+        // The VMPL value is set to 0, which means the report is generated at the highest privilege level.
+        const VMPL: u8 = 0;
+
+        // Get and verify the TSM Report path instance for SEV-SNP.
+        let tsm_report_path = TsmReportPath::new(TsmReportProvider::Sev)?;
+
+        // Generate the attestation report from the TSM Report path.
+        report_bytes = tsm_report_path
+            .attestation_report(TsmReportData::Sev(VMPL, report_data.to_vec()))?;
+    } else {
+        tracing::debug!("sysfs for SEV-SNP is not supported, which requires kernel version >= 6.16.");
+        let mut firmware = Firmware::open()?;
+        report_bytes = firmware.get_report(None, Some(report_data), Some(0))?;
+    }
+    
     let report = AttestationReport::from_bytes(&report_bytes)?;
     Ok(*report.host_data)
 }
