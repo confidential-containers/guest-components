781a7e9faa5a2b8ba3c1faf6658e884348cd248d
shashidhar naraparaju <shashidhar.naraparaju@fortanix.com>
2026-09-12 16:01:29 +0530

    coco_keyprovider: read the algorithm parameter from the right key
    Signed-off-by: shashidhar naraparaju <shashidhar.naraparaju@fortanix.com>

--- attestation-agent/coco_keyprovider/src/enc_mods/mod.rs
+++ attestation-agent/coco_keyprovider/src/enc_mods/mod.rs
@@ -82,7 +82,7 @@ fn parse_input_params(input: &str) -> Result<InputParams> {
     let keyid = map.get("keyid").map(|id| id.to_string());
     let keypath = map.get("keypath").map(|p| p.to_string());
     let algorithm = map
-        .get("keypath")
+        .get("algorithm")
         .map(|alg| (*alg).try_into().unwrap_or_default())
         .unwrap_or_default();
     Ok(InputParams {
